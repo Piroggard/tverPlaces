62245a0eb86c102112b7f2349b30f2cd67e38539
MaxF <piroggard@yandex.ru>
2023-08-16 21:22:45 +0300

    Tver places

---
+++ src/main/java/com/example/tverPlaces/TverPlacesApplication.java
@@ -0,0 +1,13 @@
+package com.example.tverPlaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TverPlacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TverPlacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tverPlaces/TverPlacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tverPlaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TverPlacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
